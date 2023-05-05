6f27586ca2c3fbc719dd3e66450fea6b4bb2db18
omerzohar1999 <omerzohar1999@gmail.com>
2023-05-05 16:55:04 +0300

    add euler path script skeleton

---
+++ degree_self_learning/semester3/algorithms/euler_path.py
@@ -0,0 +1,17 @@
+from semester2.data_structures.linked_list import MyLinkedList
+
+
+def find_euler_circle(graph: (list, list)):
+    path = MyLinkedList()
+
+
+def find_euler_path():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
