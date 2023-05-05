ad35de39fbc70f0f54d2144b30cfde8c6154ab55
omerzohar1999 <omerzohar1999@gmail.com>
2023-05-05 16:52:06 +0300

    binary heap: add value to nodes in addition to keys

--- degree_self_learning/semester2/data_structures/binary_heap.py
+++ degree_self_learning/semester2/data_structures/binary_heap.py
@@ -2,31 +2,32 @@ class MyBinaryHeap:  # TODO
     def __init__(self):
         self.heap_arr = []
 
-    def insert(self, value):
-        self.heap_arr.append(value)
+    def insert(self, value, key):
+        self.heap_arr.append((value, key))
         self.heapify_up(len(self.heap_arr) - 1)
 
     def heapify_up(self, index):
-        if not (index == 0 or self.heap_arr[index] > self.heap_arr[index // 2]):
+        if not (index == 0 or self.heap_arr[index][0] > self.heap_arr[index // 2][0]):
             self.heap_arr[index], self.heap_arr[index // 2] = self.heap_arr[index // 2], self.heap_arr[index]
             self.heapify_up(index // 2)
     
     def heapify_down(self, index):
         right = index * 2 + 1
         left = index * 2
-        if left < len(self.heap_arr):
-            if right >= len(self.heap_arr):
-                if self.heap_arr[index] < self.heap_arr[left]:
-                    self.heap_arr[index], self.heap_arr[left] = self.heap_arr[left], self.heap_arr[index]
+        heap_arr = self.heap_arr
+        if left < len(heap_arr):
+            if right >= len(heap_arr):
+                if heap_arr[index][0] < heap_arr[left][0]:
+                    heap_arr[index], heap_arr[left] = heap_arr[left], heap_arr[index]
             else:
-                if self.heap_arr[index] > self.heap_arr[right] or self.heap_arr[index] > self.heap_arr[left]:
-                    new_index = right if self.heap_arr[right] < self.heap_arr[left] else left
-                    self.heap_arr[index], self.heap_arr[new_index] = self.heap_arr[new_index], self.heap_arr[index]
+                if heap_arr[index][0] > heap_arr[right][0] or heap_arr[index][0] > heap_arr[left][0]:
+                    new_index = right if heap_arr[right][0] < heap_arr[left][0] else left
+                    heap_arr[index], heap_arr[new_index] = heap_arr[new_index], heap_arr[index]
                     self.heapify_down(new_index)
-                
-    
+
     def delete(self, index):
-        self.heap_arr[len(self.heap_arr) - 1], self.heap_arr[index] = self.heap_arr[index], self.heap_arr[len(self.heap_arr) - 1]
+        n = len(self.heap_arr)
+        self.heap_arr[n - 1], self.heap_arr[index] = self.heap_arr[index], self.heap_arr[n - 1]
         self.heap_arr.pop()
         self.heapify_down(index)
 
@@ -36,19 +37,19 @@ class MyBinaryHeap:  # TODO
         return to_return
 
 
-
 def test():
     my_heap = MyBinaryHeap()
-    my_heap.insert(9)
-    my_heap.insert(3)
-    my_heap.insert(4)
-    my_heap.insert(6)
-    my_heap.insert(2)
+    my_heap.insert(9, 0)
+    my_heap.insert(3, 0)
+    my_heap.insert(4, 0)
+    my_heap.insert(6, 0)
+    my_heap.insert(2, 0)
 
     print(my_heap.pop_min())
     print(my_heap.pop_min())
     print(my_heap.pop_min())
     print(my_heap.pop_min())
     print(my_heap.pop_min())
+
 
 test()
